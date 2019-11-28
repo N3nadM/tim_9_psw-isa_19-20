0f58a19d4907da830528ee976cf18c4f3416b2dd
Igor Gligorevic <igor.gligorevic@hotmail.com>
2019-11-28 18:14:17 +0100

    implementiranje pretrage i uvezivanje sa backom

--- client/src/store/actions/klinika.js
+++ client/src/store/actions/klinika.js
@@ -9,22 +9,34 @@ export const setKlinike = klinike => ({
 export const setNewKlinika = newKlinika => ({
   type: SET_ADDED_KLINIKA,
   newKlinika
-})
+});
 
-export const getAllKlinike = (sum, rpp) => async dispatch => {
+export const getAllKlinike = (sum, rpp) => async (dispatch, getState) => {
   try {
-    const klinike = await axios.get(`/api/klinika`);
+    let klinike = getState().klinika.klinike;
+    if (!klinike) {
+      klinike = await axios.get(`/api/klinika`);
+      dispatch(setKlinike(klinike.data));
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const searchKlinike = searchData => async dispatch => {
+  try {
+    const klinike = await axios.post(`/api/klinika/search`, searchData);
     dispatch(setKlinike(klinike.data));
   } catch (err) {
     console.log(err);
   }
 };
 
-export const addNewKlinika = (data) => async dispatch => {
+export const addNewKlinika = data => async dispatch => {
   try {
-    const klinika = await axios.post('/api/klinika', data);
+    const klinika = await axios.post("/api/klinika", data);
     dispatch(setNewKlinika(klinika.data));
   } catch (err) {
     console.log(err);
   }
-}
+};
